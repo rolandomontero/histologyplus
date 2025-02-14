392364c61a773aca86b4337de4bb5fb74eea7796
rolandomontero <rolandomontero@hotmail.com>
2025-02-14 16:25:34 -0300

    Voy almorzar  y descanzar

--- public/assets/dist/js/main.js
+++ public/assets/dist/js/main.js
@@ -1,6 +1,7 @@
 // Capturar el botón
 const btnTop = document.getElementById("btnTop");
 const imgHistology = document.getElementById("imgHistology");
+const imgAprende = document.getElementById("imgAprende");
 
 // Mostrar u ocultar el botón según el scroll
 window.addEventListener("scroll", function () {
@@ -22,19 +23,39 @@ btnTop.addEventListener("click", function () {
   });
 });
 
-window.addEventListener("resize", function () {
-  console.log('Cambiando imagen:', window.innerWidth < 990 ? 'mobile' : 'desktop');
-  if(window.innerWidth < 990){
-    imgHistology.src="./assets/images/CallToActionMobile.png";
-    //imgHistology.style.width = "100%";
-  }else{
-    imgHistology.src="./assets/images/iPhone02.png";
-    //imgHistology.style.width = "70%";
-  }
-   
-});
+window.addEventListener("resize", imgResize);
 
 window.onload = function () {
   window.scrollTo(0, 0);
- 
+  imgResize();
 };
+
+function imgResize() {
+  if (window.innerWidth < 992) {
+    imgHistology.src = "./assets/images/CallToActionMobile.png";
+    imgHistology.style.width = "100%";
+    imgAprende.src = "./assets/images/ipad.png";
+    imgAprende.style.width = "100%";
+  } else {
+    imgHistology.src = "./assets/images/iPhone02.png";
+    imgAprende.src = "./assets/images/iPhone03.png";
+  }
+}
+
+(() => {
+  "use strict";
+  document
+    .querySelector("#navbarSideCollapse")
+    .addEventListener("click", () => {
+      document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
+})();
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const mensaje = document.getElementById("mensaje").value;
+  const mailtoLink = `mailto:info@histologyplus.com?subject=Quiero%20informaci%C3%B3n&body=${encodeURIComponent(
+    mensaje
+  )}`;
+  window.location.href = mailtoLink;
+});
