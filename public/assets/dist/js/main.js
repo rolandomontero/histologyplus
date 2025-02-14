0b78aad88992698dd7c8f6595f4c843b24b83585
rolandomontero <rolandomontero@hotmail.com>
2025-02-14 12:07:51 -0300

    Redimensionamiento del CTA

--- public/assets/dist/js/main.js
+++ public/assets/dist/js/main.js
@@ -1,19 +1,40 @@
- // Capturar el botón
- const btnTop = document.getElementById("btnTop");
+// Capturar el botón
+const btnTop = document.getElementById("btnTop");
+const imgHistology = document.getElementById("imgHistology");
 
- // Mostrar u ocultar el botón según el scroll
- window.addEventListener("scroll", function() {
-     if (window.scrollY > 300) {
-         btnTop.style.display = "block";
-     } else {
-         btnTop.style.display = "none";
-     }
- });
+// Mostrar u ocultar el botón según el scroll
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    btnTop.style.display = "block";
+    btnTop.style.transition =
+      "transform 1s ease-in-out, visibility 1s ease-in-out";
+    // transition: transform 0.3s ease-in-out, visibility 0.3s ease-in-out;
+  } else {
+    btnTop.style.display = "none";
+  }
+});
 
- // Volver al inicio con animación suave
- btnTop.addEventListener("click", function() {
-     window.scrollTo({
-         top: 0,
-         behavior: "smooth"
-     });
- });
+// Volver al inicio con animación suave
+btnTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("resize", function () {
+  console.log('Cambiando imagen:', window.innerWidth < 990 ? 'mobile' : 'desktop');
+  if(window.innerWidth < 990){
+    imgHistology.src="./assets/images/CallToActionMobile.png";
+    //imgHistology.style.width = "100%";
+  }else{
+    imgHistology.src="./assets/images/iPhone02.png";
+    //imgHistology.style.width = "70%";
+  }
+   
+});
+
+window.onload = function () {
+  window.scrollTo(0, 0);
+ 
+};
